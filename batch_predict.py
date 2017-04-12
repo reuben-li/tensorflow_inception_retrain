f5f6bc5adb0ea3be8278bc29960ad6276a7d4d04
Reuben Li <reuben.li@fastretailing.com>
2017-04-12 17:21:44 +0900

    Loop through images in directory; Single label

--- batch_predict.py
+++ batch_predict.py
@@ -1,26 +1,46 @@
 import numpy as np
 import tensorflow as tf
-import glob
 import os
-graph_path = '/tmp/output_graph.pb'
-image_dir = 'test_data/*/*.jpg'
 
-def load_graph():
-    with tf.gfile.FastGFile(graph_path, 'rb') as graph:
+testImagePath = './test_data'
+modelFullPath = '/tmp/output_graph.pb'
+labelsFullPath = '/tmp/output_labels.txt'
+
+def create_graph():
+    with tf.gfile.FastGFile(modelFullPath, 'rb') as f:
         graph_def = tf.GraphDef()
-        graph_def.ParseFromString(graph.read())
+        graph_def.ParseFromString(f.read())
         _ = tf.import_graph_def(graph_def, name='')
 
-testimages=glob.glob(image_dir)
-all_predictions = np.zeros(shape=(len(testimages), 25))
-load_graph()
+def run_inference_on_image():
+    answer = None
 
-with tf.Session() as sess:
-    softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
-    for i in range(len(testimages)):
-        image_data = tf.gfile.FastGFile(testimages[i], 'rb').read()
-        predictions = sess.run(softmax_tensor,
-                {'DecodeJpeg/contents:0': image_data})
-        all_predictions[i,:] = np.squeeze(predictions)
-        if i % 100 == 0:
-            print(str(i) +' of a total of '+ str(len(testimages)))
+    create_graph()
+
+    with tf.Session() as sess:
+        for subd in os.listdir(testImagePath):
+            subPath = os.path.join(testImagePath, subd)
+            for image in os.listdir(subPath):
+                oneImage = os.path.join(subPath, image)
+                image_data = tf.gfile.FastGFile(oneImage, 'rb').read()
+
+                softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+                predictions = sess.run(softmax_tensor,
+                                       {'DecodeJpeg/contents:0': image_data})
+                predictions = np.squeeze(predictions)
+
+                top_k = predictions.argsort()[-1:][::-1]  # Getting top 5 predictions
+                f = open(labelsFullPath, 'rb')
+                lines = f.readlines()
+                labels = [str(w).replace("\n", "") for w in lines]
+                for node_id in top_k:
+                    human_string = labels[node_id]
+                    score = predictions[node_id]
+                    # print('%s (score = %.5f)' % (human_string, score))
+
+                answer = labels[top_k[0]]
+                print answer
+        return answer
+
+if __name__ == '__main__':
+    print run_inference_on_image()
