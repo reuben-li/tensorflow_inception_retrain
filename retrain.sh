49349b982385a078f773b6d87bf1bf70336b1e4b
Reuben Li <mesolithic@gmail.com>
2017-04-17 07:38:15 +0000

    Added bazel installation and init cond

--- retrain.sh
+++ retrain.sh
@@ -5,12 +5,20 @@ MODEL_PATH="../model"
 BOTTLENECK_PATH="../bottleneck"
 GRAPH_PATH="../graph"
 LABELS_PATH="../labels"
+INIT=true
 
-# Compile retrain function in tensorflow master
-git clone https://github.com/tensorflow/tensorflow.git
-cd tensorflow
-./configure
-bazel build tensorflow/examples/image_retraining:retrain
+if [  "$INIT" = true ]; then
+  # install bazel (ubuntu)
+  echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+  curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+  sudo apt-get update && sudo apt-get install bazel
+
+  # Compile retrain function in tensorflow master
+   git clone https://github.com/tensorflow/tensorflow.git
+   cd tensorflow
+   ./configure
+   bazel build tensorflow/examples/image_retraining:retrain
+fi
 
 # Retraining with proper image directory
 bazel-bin/tensorflow/examples/image_retraining/retrain \
